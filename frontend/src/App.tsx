744d0659f213b25f775d89c3c6db8b0edfa69b46
sumit <sumitjadala17@gmail.com>
2025-08-01 23:14:07 +0530

    inital commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage';
+import LandingPage from './pages/LandingPage';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+// import React from "react";
+
+const LandingPage = () => {
+    return (
+        <div>This is LandingPage.....</div>
+    );
+}
+
+
+export default LandingPage;
